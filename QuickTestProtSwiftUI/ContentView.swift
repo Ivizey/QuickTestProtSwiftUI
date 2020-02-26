19ec1af4b3d25968a784eef756c332aab2cb10f6
Pavel Bondar <ivizey@gmail.com>
2020-02-26 13:43:23 +0200

    add test card

--- QuickTestProtSwiftUI/ContentView.swift
+++ QuickTestProtSwiftUI/ContentView.swift
@@ -10,7 +10,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Card(title: "Three")
+                .blendMode(.hardLight)
+                .padding(64)
+                .padding(.bottom, 64)
+            Card(title: "Second")
+                .blendMode(.hardLight)
+                .padding(32)
+                .padding(.bottom, 32)
+            MainCard(title: "Main card")
+        }
+    }
+}
+
+struct Card: View {
+    var title: String
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color.init(red: 68/255, green: 41/255, blue: 182/255))
+                .frame(height: 230)
+                .cornerRadius(10)
+                .padding(16)
+            Text(title)
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .bold()
+        }
+    }
+}
+
+struct MainCard: View {
+    var title: String
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color.black)
+                .frame(height: 230)
+                .cornerRadius(10)
+                .padding(16)
+            Text(title)
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .bold()
+        }
     }
 }
 
