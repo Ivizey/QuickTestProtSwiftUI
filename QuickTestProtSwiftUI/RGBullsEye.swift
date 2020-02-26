b445afced93dbd685ae45a07f2a2ff16da7de36f
Pavel Bondar <ivizey@gmail.com>
2020-02-26 18:05:38 +0200

    colorize rectangle

--- QuickTestProtSwiftUI/RGBullsEye.swift
+++ QuickTestProtSwiftUI/RGBullsEye.swift
@@ -9,26 +9,40 @@
 import SwiftUI
 
 struct RGBullsEye: View {
-  var body: some View {
-    HStack {
-      VStack {
-        Rectangle()
-        Text("Match this color")
-      }
-      VStack {
-        Rectangle()
+    let rTarget = Double.random(in: 0..<1)
+    let gTarget = Double.random(in: 0..<1)
+    let bTarget = Double.random(in: 0..<1)
+    @State var rGuess: Double
+    @State var gGuess: Double
+    @State var bGuess: Double
+    var body: some View {
         HStack {
-          Text("R: xxx")
-          Text("G: xxx")
-          Text("B: xxx")
+            VStack {
+                Rectangle()
+                .foregroundColor(Color(red: rTarget,
+                                       green: gTarget,
+                                       blue: bTarget,
+                                       opacity: 1.0))
+                Text("Match this color")
+            }
+            VStack {
+                Rectangle()
+                .foregroundColor(Color(red: rTarget,
+                                       green: gTarget,
+                                       blue: bTarget,
+                                       opacity: 1.0))
+                HStack {
+                    Text("R: xxx")
+                    Text("G: xxx")
+                    Text("B: xxx")
+                }
+            }
         }
-      }
     }
-  }
 }
 
 struct RGBullsEye_Previews: PreviewProvider {
-  static var previews: some View {
-    RGBullsEye()
-  }
+    static var previews: some View {
+        RGBullsEye(rGuess: 0.5, gGuess: 0.5, bGuess: 0.5)
+    }
 }
--- QuickTestProtSwiftUI/SceneDelegate.swift
+++ QuickTestProtSwiftUI/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
-        let contentView = ContentView()
+        let contentView = RGBullsEye(rGuess: 0.5, gGuess: 0.5, bGuess: 0.5)
 
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
