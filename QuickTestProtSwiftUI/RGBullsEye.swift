e477d6d4646627c0d33723229c0837bee314f3d1
Pavel Bondar <ivizey@gmail.com>
2020-02-26 18:39:34 +0200

    add RGB sliders

--- QuickTestProtSwiftUI/RGBullsEye.swift
+++ QuickTestProtSwiftUI/RGBullsEye.swift
@@ -16,26 +16,34 @@ struct RGBullsEye: View {
     @State var gGuess: Double
     @State var bGuess: Double
     var body: some View {
-        HStack {
-            VStack {
-                Rectangle()
-                .foregroundColor(Color(red: rTarget,
-                                       green: gTarget,
-                                       blue: bTarget,
-                                       opacity: 1.0))
-                Text("Match this color")
-            }
-            VStack {
-                Rectangle()
-                .foregroundColor(Color(red: rTarget,
-                                       green: gTarget,
-                                       blue: bTarget,
-                                       opacity: 1.0))
-                HStack {
-                    Text("R: xxx")
-                    Text("G: xxx")
-                    Text("B: xxx")
+        VStack {
+            HStack {
+                VStack {
+                    Rectangle()
+                        .foregroundColor(Color(red: rTarget,
+                                               green: gTarget,
+                                               blue: bTarget,
+                                               opacity: 1.0))
+                    Text("Match this color")
                 }
+                VStack {
+                    Rectangle()
+                        .foregroundColor(Color(red: rTarget,
+                                               green: gTarget,
+                                               blue: bTarget,
+                                               opacity: 1.0))
+                    HStack {
+                        Text("R: \(Int(rGuess * 255.0))")
+                        Text("G: \(Int(gGuess * 255.0))")
+                        Text("B: \(Int(bGuess * 255.0))")
+                    }
+                }
+            }
+            Text("Hit me button")
+            VStack {
+                ColorSlider(value: $rGuess, textColor: .red)
+                ColorSlider(value: $gGuess, textColor: .green)
+                ColorSlider(value: $bGuess, textColor: .blue)
             }
         }
     }
@@ -44,5 +52,20 @@ struct RGBullsEye: View {
 struct RGBullsEye_Previews: PreviewProvider {
     static var previews: some View {
         RGBullsEye(rGuess: 0.5, gGuess: 0.5, bGuess: 0.5)
+    }
+}
+
+struct ColorSlider: View {
+    @Binding var value: Double
+    var textColor: Color
+    var body: some View {
+        HStack {
+            Text("0")
+                .foregroundColor(textColor)
+            Slider(value: $value)
+            Text("255")
+                .foregroundColor(textColor)
+        }
+        .padding()
     }
 }
