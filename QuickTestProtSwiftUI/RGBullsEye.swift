06badfe8403ee4b2b7e724e50e170619e609c425
Pavel Bondar <ivizey@gmail.com>
2020-02-26 17:39:28 +0200

    create RGBullsEye file

---
+++ QuickTestProtSwiftUI/RGBullsEye.swift
@@ -0,0 +1,21 @@
+//
+//  RGBullsEye.swift
+//  QuickTestProtSwiftUI
+//
+//  Created by Pavel Bondar on 26.02.2020.
+//  Copyright © 2020 Pavel Bondar. All rights reserved.
+//
+
+import SwiftUI
+
+struct RGBullsEye: View {
+  var body: some View {
+    Text("Hello World")
+  }
+}
+
+struct RGBullsEye_Previews: PreviewProvider {
+  static var previews: some View {
+    RGBullsEye()
+  }
+}
