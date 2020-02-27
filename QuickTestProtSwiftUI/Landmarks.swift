4e8b6d671381a4998ad221825d1c757728df0fae
Pavel Bondar <ivizey@gmail.com>
2020-02-27 14:15:48 +0200

    create Landmarks file

---
+++ QuickTestProtSwiftUI/Landmarks.swift
@@ -0,0 +1,21 @@
+//
+//  Landmarks.swift
+//  QuickTestProtSwiftUI
+//
+//  Created by Pavel Bondar on 27.02.2020.
+//  Copyright © 2020 Pavel Bondar. All rights reserved.
+//
+
+import SwiftUI
+
+struct Landmarks: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct Landmarks_Preview: PreviewProvider {
+    static var previews: some View {
+        Landmarks()
+    }
+}
