a3ef60c3566bf51e55d3d38426949168a97abc82
Pavel Bondar <ivizey@gmail.com>
2020-03-01 13:50:17 +0200

    add navigation between sections

--- QuickTestProtSwiftUI/Landmark/Landmark Views/Home.swift
+++ QuickTestProtSwiftUI/Landmark/Landmark Views/Home.swift
@@ -12,7 +12,7 @@ struct CategoryHome: View {
     var categories: [String: [Landmark]] {
         Dictionary(
             grouping: landmarkData,
-            by: { $0.category.rawValue}
+            by: { $0.category.rawValue }
         )
     }
     
@@ -20,6 +20,17 @@ struct CategoryHome: View {
         landmarkData.filter { $0.isFeatured }
     }
     
+    @State var showingProfile = false
+    
+    var profileButton: some View {
+        Button(action: { self.showingProfile.toggle() }) {
+            Image(systemName: "person.crop.circle")
+                .imageScale(.large)
+                .accessibility(label: Text("User Profile"))
+                .padding()
+        }
+    }
+
     var body: some View {
         NavigationView {
             List {
@@ -27,11 +38,22 @@ struct CategoryHome: View {
                     .scaledToFill()
                     .frame(height: 200)
                     .clipped()
+                    .listRowInsets(EdgeInsets())
+                
                 ForEach(categories.keys.sorted(), id: \.self) { key in
                     CategoryRow(categoryName: key, items: self.categories[key]!)
                 }
+                .listRowInsets(EdgeInsets())
+                
+                NavigationLink(destination: LandmarkList()) {
+                    Text("See All")
+                }
             }
             .navigationBarTitle(Text("Featured"))
+            .navigationBarItems(trailing: profileButton)
+            .sheet(isPresented: $showingProfile) {
+                Text("User Profile")
+            }
         }
     }
 }
