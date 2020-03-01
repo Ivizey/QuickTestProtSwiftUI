1fc976c9e4d007777284c3b62ffb7b240101d46b
Pavel Bondar <ivizey@gmail.com>
2020-03-01 17:57:12 +0200

    delay edit propagation

--- QuickTestProtSwiftUI/Profile/ProfileHost.swift
+++ QuickTestProtSwiftUI/Profile/ProfileHost.swift
@@ -13,10 +13,17 @@ struct ProfileHost: View {
     @Environment(\.editMode) var mode
     @EnvironmentObject var userData: UserData
     @State var draftProfile = Profile.default
-
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
             HStack {
+                if self.mode?.wrappedValue == .active {
+                    Button("Cancel") {
+                        self.draftProfile = self.userData.profile
+                        self.mode?.animation().wrappedValue = .inactive
+                    }
+                }
+                
                 Spacer()
                 
                 EditButton()
@@ -25,6 +32,12 @@ struct ProfileHost: View {
                 ProfileSummary(profile: userData.profile)
             } else {
                 ProfileEditor(profile: $draftProfile)
+                    .onAppear {
+                        self.draftProfile = self.userData.profile
+                }
+                .onDisappear {
+                    self.userData.profile = self.draftProfile
+                }
             }
         }
         .padding()
