13a768a3d9a2dc9208bef64fa86becc76c0c0742
Pavel Bondar <ivizey@gmail.com>
2020-03-05 14:31:18 +0200

    add card data view

---
+++ QuickTestProtSwiftUI/CardDataView.swift
@@ -0,0 +1,21 @@
+//
+//  CardDataView.swift
+//  QuickTestProtSwiftUI
+//
+//  Created by Pavel Bondar on 05.03.2020.
+//  Copyright © 2020 Pavel Bondar. All rights reserved.
+//
+
+import SwiftUI
+
+struct CardDataView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardDataView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardDataView()
+    }
+}
--- QuickTestProtSwiftUI/SignInView.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SignInView.swift
-//  QuickTestProtSwiftUI
-//
-//  Created by Pavel Bondar on 04.03.2020.
-//  Copyright © 2020 Pavel Bondar. All rights reserved.
-//
-
-import Foundation
